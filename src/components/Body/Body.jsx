f35684b8245fb7c065b5f03d2e58a80a51b94a6c
HadeSatanist <ap66484@gmail.com>
2023-01-23 17:42:05 -0300

    Refatoração do codigo para implementar o componente de carrossel no body

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import styles from './Body.module.css'
 import Produtos from '../Produtos/Produtos'
+import Carrossel from '../Carrossel/Carrossel'
+
+
 
 const Body = ()=>{
     return(<div className={styles.body}>
-        <div className={styles.categorias}>
+
             <div className={styles.titulo}>Novidades</div>
-                <Produtos/>
-        </div >
-        <div className={styles.categorias}>
-            <div className={styles.titulo}>Populares</div>
-                <Produtos/>
-        </div>
-        <div className={styles.categorias}>
-            <div className={styles.titulo}>Clássicos</div>
-                <Produtos/>
-        </div>
-        <div className={styles.categorias}>
-            <div className={styles.titulo}>Ofertas</div>
-                <Produtos/>
-        </div>
+                <Carrossel></Carrossel>
         
+            <div className={styles.titulo}>Populares</div>
+                <Carrossel></Carrossel>
+
+            <div className={styles.titulo}>Clássicos</div>
+                <Carrossel></Carrossel>
+
+            <div className={styles.titulo}>Ofertas</div>
+                <Carrossel></Carrossel>
+                
         </div>
     )
 
