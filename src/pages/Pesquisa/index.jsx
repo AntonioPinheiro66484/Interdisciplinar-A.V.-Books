29e0225ff4fcb44f2e6a4324dbbaeedebb47ccef
HadeSatanist <ap66484@gmail.com>
2023-01-25 15:59:29 -0300

    componente pesquisa criado

---
+++ src/pages/Pesquisa/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Pesquisa.module.css"
+import Header from "../../components/Header/Header";
+import NavBar from "../../components/NavBar/NavBar";
+import Body from "../../components/Body/Body";
+import PreFooter from "../../components/Footer/Prefooter/PreFooter";
+import Footer from "../../components/Footer/Footer";
+
+const Pesquisa =()=>{
+    return(
+        <div>
+            <Header></Header>
+            <NavBar></NavBar>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default Pesquisa
