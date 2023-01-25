ca4695ffbbc7e1db93802f6038f74c38edb6720e
HadeSatanist <ap66484@gmail.com>
2023-01-25 15:56:54 -0300

    implementação das rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
 import Header from './components/Header/Header'
 import NavBar from './components/NavBar/NavBar'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Body from './components/Body/Body'
 import PreFooter from './components/Footer/Prefooter/PreFooter'
 import Footer from './components/Footer/Footer'
-import Carrossel from './components/Carrossel/Carrossel';
+import Carrossel from './components/Carrossel/Carrossel'
+import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
+import DetalhesProduto from './pages/DetalhesProduto'
+import Pesquisa from './pages/Pesquisa'
+
 
 function App() {
   return (
     <div className="App">
-       <Header></Header>
-        <NavBar></NavBar>
-        <Body></Body>
-        <PreFooter></PreFooter>
-        <Footer></Footer>
-
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/DetalhesProduto" element={<DetalhesProduto/>}/>
+              <Route path="/Pesquisa" element={<Pesquisa/>}/>
+              <Route path="/Cadastro" element={<Cadastro/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
