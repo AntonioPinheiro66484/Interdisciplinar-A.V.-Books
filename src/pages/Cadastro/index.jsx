f1dd0d7b3afd54208652e4cc24db2fa487a68e4c
HadeSatanist <ap66484@gmail.com>
2023-01-26 15:41:30 -0300

    index do cadastro

---
+++ src/pages/Cadastro/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Formulario from "../../components/Cadastro/Formulario/Formulario";
+import Footer from "../../components/Footer/Footer";
+import PreFooter from "../../components/Footer/Prefooter/PreFooter";
+import Header from "../../components/Header/Header";
+import NavBar from "../../components/NavBar/NavBar";
+import styles from "./Cadastro.module.css"
+
+const Cadastro =()=>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Cadastro
