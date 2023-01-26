69f8d0f7907956bda41399ae9a83ee9b1da8217a
Spideyig <igor01mj@gmail.com>
2023-01-26 16:22:55 -0300

    Começo da página de pesquisa

--- src/pages/Pesquisa/index.jsx
+++ src/pages/Pesquisa/index.jsx
@@ -5,12 +5,20 @@ import NavBar from "../../components/NavBar/NavBar";
 import Body from "../../components/Body/Body";
 import PreFooter from "../../components/Footer/Prefooter/PreFooter";
 import Footer from "../../components/Footer/Footer";
+import Produtos from "../../components/Produtos/Produtos";
 
 const Pesquisa =()=>{
     return(
         <div>
             <Header></Header>
             <NavBar></NavBar>
+             <div className={styles.produtosPesquisa}>
+                    <Produtos/>
+                    <Produtos/>
+                    <Produtos/>
+                    <Produtos/>
+             </div>
+            <PreFooter></PreFooter>
             <Footer></Footer>
         </div>
     )
