192fb18088f5af6925ee19daded2e8bb39dd6043
HadeSatanist <ap66484@gmail.com>
2023-02-10 17:29:32 -0300

    alterações na navbar e no menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import styles from './NavBar.module.css'
+import MenuImg from '../../assets/icons/menu.png'
 
 const NavBar = () => {
   return (
   
       <nav className={styles.navBar}>
+        <div className={styles.caixabotaomenu}>
+            <div className={styles.botao}>
+              <i src={MenuImg}></i>
+            </div>
+         
+        </div>
             <ul className={styles.paiItems}>
               <li className={styles.item}><a  className={styles.link} href="#">Best Sellers</a></li>
               <li className={styles.item}><a  className={styles.link} href="#">E-Books</a></li>
