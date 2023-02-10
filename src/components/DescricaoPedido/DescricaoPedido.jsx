e5da62b2236e1bba705d0a4d2f70020a8ad2516b
vinivallim <vinivallim@hotmail.com>
2023-02-10 16:01:53 -0300

    melhoria responsiva

--- src/components/DescricaoPedido/DescricaoPedido.jsx
+++ src/components/DescricaoPedido/DescricaoPedido.jsx
@@ -9,8 +9,10 @@ const Pedido = () => {
       <div className={styles.tituloproduto}>O diário perdido de Gravity falls</div>
       <div className={styles.editora}>Universo dos Livros</div>
       <div className={styles.preco}>R$ 63,90</div>
+      <div className={styles.botao}>
       <BotaoAdicionar/>
       <BotaoComprar/>
+      </div>
       <div className={styles.localizacao}>Onde você está?</div>
       <div className={styles.localizacaodescricao}>Informe sua localização para calcularmos os valores de frete e localizarmos possíveis pontos de retirada.</div>
       <div className={styles.inputs}>
