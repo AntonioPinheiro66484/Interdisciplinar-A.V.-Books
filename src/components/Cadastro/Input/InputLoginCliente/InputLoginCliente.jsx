1a80ca20525e8776d36626639c213d592ffc626d
HadeSatanist <ap66484@gmail.com>
2023-02-16 17:29:54 -0300

    estilizando a pagina de cliente

---
+++ src/components/Cadastro/Input/InputLoginCliente/InputLoginCliente.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './InputLoginCliente.module.css'
+import AvBookslogo from '../../../../assets/images/Imagens/logo.png'
+function InputLoginCliente() {
+  return (
+    <div className={styles.caixa}>
+      <div className={styles.caixaLogo}>
+         <img className={styles.logo} src={AvBookslogo}/>
+         <div className={styles.titulosLogoSite}>A.V. Books</div>
+         <div className={styles.texto}>Faça login e comece a ler!</div>
+       </div>
+    <div className={styles.inputs}>
+            <div className={styles.campoTexto}>
+                <label class={styles.label}>Digite seu endereço de e-mail</label>
+                <input className={styles.input} type="email" placeholder="johndoe@example.com"></input>
+            </div>
+
+            <div className={styles.campoTexto}>
+                <label class={styles.label}>Digite sua senha</label>
+                <input className={styles.input} type="password" placeholder="***************"></input>
+            </div>
+    </div>
+    </div>
+  )
+}
+
+export default InputLoginCliente
