9c2fcf4416f35072201744fb54e5ea1ea356c61d
HadeSatanist <ap66484@gmail.com>
2023-02-17 08:58:40 -0300

    atualização de rotas para o projeto

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import DetalhesProduto from './pages/DetalhesProduto'
 import Pesquisa from './pages/Pesquisa'
 import Administracao from './pages/Administracao'
 import LoginCliente from './pages/LoginCliente/LoginCliente';
-
+import LoginAdm from './pages/LoginADM/LoginADM';
 
 function App() {
   return (
@@ -19,6 +19,8 @@ function App() {
               <Route path="/Cadastro" element={<Cadastro/>}/>
               <Route path="/Administracaof" element={<Administracao/>}/>
               <Route path="/LoginCliente" element={<LoginCliente/>}/>
+              <Route path="/LoginAdm" element={<LoginAdm/>}/>
+
           </Routes>
       </Router>
     </div>
