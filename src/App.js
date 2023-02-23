06468d0d59433769cd6e66e3723a4e17aa010992
HadeSatanist <ap66484@gmail.com>
2023-02-23 18:23:48 -0300

    Adição de novas rotas de pedido e fornecedor

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Cadastro from './pages/Cadastro'
 import CadastroLivro from './pages/CadastroLivro'
+import CadastroFornecedor from './pages/CadastroFornecedor'
+import CadastroPedido from './pages/CadastroPedido'
 import DetalhesProduto from './pages/DetalhesProduto'
 import Pesquisa from './pages/Pesquisa'
 import Administracao from './pages/Administracao'
 import LoginCliente from './pages/LoginCliente/LoginCliente';
 import LoginAdm from './pages/LoginADM/LoginADM';
+
 
 function App() {
   return (
@@ -19,6 +22,8 @@ function App() {
               <Route path="/Pesquisa" element={<Pesquisa/>}/>
               <Route path="/Cadastro" element={<Cadastro/>}/>
               <Route path="/CadastroLivro" element={<CadastroLivro/>}/>
+              <Route path="/CadastroFornecedor" element={<CadastroFornecedor/>}/>
+              <Route path="/CadastroPedido" element={<CadastroPedido/>}/>
               <Route path="/Administracao" element={<Administracao/>}/>
               <Route path="/LoginCliente" element={<LoginCliente/>}/>
               <Route path="/LoginAdm" element={<LoginAdm/>}/>
