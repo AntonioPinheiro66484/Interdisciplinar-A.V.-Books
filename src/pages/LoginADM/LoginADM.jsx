f8b7a05b7a14ca4a2171b190b78f47f3e0f48f3a
HadeSatanist <ap66484@gmail.com>
2023-02-27 16:28:54 -0300

    Ajustes na Pagina de login

--- src/pages/LoginADM/LoginADM.jsx
+++ src/pages/LoginADM/LoginADM.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Acessibilidade from '../../components/Acessibilidade/Acessibilidade'
 import InputLoginAdm from '../../components/Cadastro/Input/InputLoginAdm/InputLoginAdm'
 import styles from './LoginADM.module.css'
 function LoginADM() {
   return (
     
       <div className={styles.caixa}>
-        <Acessibilidade></Acessibilidade>
         <div className={styles.inputADM}>
           <InputLoginAdm></InputLoginAdm>
         </div>
--- src/pages/LoginCliente/LoginCliente.jsx
+++ src/pages/LoginCliente/LoginCliente.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Acessibilidade from '../../components/Acessibilidade/Acessibilidade'
 import InputLoginCliente from '../../components/Cadastro/Input/InputLoginCliente/InputLoginCliente'
 import styles from './LoginCliente.module.css'
 function LoginCliente() {
   return (
     <div className={styles.caixa}>
-      <Acessibilidade></Acessibilidade>
       <div className={styles.InputCliente}>
        <InputLoginCliente></InputLoginCliente>
       </div>
