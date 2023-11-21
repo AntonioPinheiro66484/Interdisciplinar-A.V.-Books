9bc135f4e49da921831866e26e56ab9234f56808
Spideyig <igor01mj@gmail.com>
2023-11-21 16:45:36 -0300

    Página de Adm

--- src/pages/Administracao/index.jsx
+++ src/pages/Administracao/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import styles from './Administracao.module.css'
-import HeaderADM from "../../components/HeaderADM";
+import Header from "../../components/Header/index";
 import NavBarADM from "../../components/NavBarADM";
-
 import Footer from "../../components/Footer";
 import Acessibilidade from "../../components/Acessibilidade/index"
 import BotaoCharts from '../../components/Botoes/Adm/BotaoCharts'
@@ -12,11 +11,12 @@ import BotaoTables from '../../components/Botoes/Adm/BotaoTables'
 import BotaoCalendar from '../../components/Botoes/Adm/BotaoCalendar'
 import BotaoErrors from '../../components/Botoes/Adm/BotaoErrors'
 import SubFooter from '../../components/Footer/Subfooter'
+import PreFooter from '../../components/Footer/Prefooter'
 
 const Administracao = ()=>{
     return(
         <div>
-        <HeaderADM/>
+        <Header/>
         <div className={styles.botao}>
             <BotaoCharts/>
             <BotaoDashboard/>
